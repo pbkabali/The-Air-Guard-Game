5a1d2c07e5493d8fd7f3ceed1ae36add1916a1a9
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-12-24 11:06:50 +0300

    Platform game example

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          { loader: "file-loader" },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              // Compresses JPEG images
+              mozjpeg: {
+                progressive: true,
+              },
+              // Compresses PNG images
+              optipng: {
+                enabled: false,
+              },
+              // Compresses PNG images
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              // Compresses GIF images
+              ifsicle: {
+                interlaced: false,
+              },
+              // Compresses JPG and PNG into WEBP
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
