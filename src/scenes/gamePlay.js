51065086d78fea65093dcabce82b9d0f86ecd4c5
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-01-06 00:14:26 +0300

    Break up the entities file for better linting

--- src/scenes/gamePlay.js
+++ src/scenes/gamePlay.js
@@ -1,52 +1,54 @@
-import Phaser from 'phaser';
-import bg from '../assets/full-bg.png';
-import airplane from '../assets/airplane.png';
-import launcher from '../assets/launcher.png';
-import missile from '../assets/Missile04N.png';
-import bomb from '../assets/bomb.png';
-import sprExplosion from '../assets/sprExplosion.png';
-import sndExplode from '../assets/sndExplode.wav';
-import sndMissile from '../assets/sndMissile.wav';
-import applyFrustumCulling from '../helpers/frustumCulling';
-import Player, { StrayPlane, Missile } from '../helpers/entities';
+import Phaser from "phaser";
+import bg from "../assets/full-bg.png";
+import airplane from "../assets/airplane.png";
+import launcher from "../assets/launcher.png";
+import missile from "../assets/Missile04N.png";
+import bomb from "../assets/bomb.png";
+import sprExplosion from "../assets/sprExplosion.png";
+import sndExplode from "../assets/sndExplode.wav";
+import sndMissile from "../assets/sndMissile.wav";
+import Missile from "../helpers/missile";
+import StrayPlane from "../helpers/strayPlane";
+import Player from "../helpers/player";
+import applyFrustumCulling from "../helpers/frustumCulling";
 
 class GamePlay extends Phaser.Scene {
   constructor() {
-    super({ key: 'GamePlay' });
+    super({ key: "GamePlay" });
     this.score = 0;
   }
 
   preload() {
-    this.load.spritesheet('sprExplosion', sprExplosion, {
+    this.load.spritesheet("sprExplosion", sprExplosion, {
       frameWidth: 32,
       frameHeight: 32,
     });
-    this.load.image('bg', bg);
-    this.load.image('airplane', airplane);
-    this.load.image('rocketLauncher', launcher);
-    this.load.image('playerMissile', missile);
-    this.load.image('enemyBomb', bomb);
-    this.load.audio('sndExplode', sndExplode);
-    this.load.audio('sndMissile', sndMissile);
+    this.load.image("bg", bg);
+    this.load.image("airplane", airplane);
+    this.load.image("rocketLauncher", launcher);
+    this.load.image("playerMissile", missile);
+    this.load.image("enemyBomb", bomb);
+    this.load.audio("sndExplode", sndExplode);
+    this.load.audio("sndMissile", sndMissile);
   }
 
   create() {
     this.anims.create({
-      key: 'sprExplosion',
-      frames: this.anims.generateFrameNumbers('sprExplosion'),
+      key: "sprExplosion",
+      frames: this.anims.generateFrameNumbers("sprExplosion"),
       frameRate: 20,
       repeat: 0,
     });
 
     this.sfx = {
-      explosion: this.sound.add('sndExplode'),
-      laser: this.sound.add('sndMissile'),
+      explosion: this.sound.add("sndExplode"),
+      laser: this.sound.add("sndMissile"),
     };
 
     const bg = this.add.image(
       this.game.config.width / 2,
       this.game.config.height / 2,
-      'bg',
+      "bg"
     );
     bg.setScale(0.5);
 
@@ -62,11 +64,11 @@ class GamePlay extends Phaser.Scene {
           this.game.config.width,
           Phaser.Math.Between(
             this.game.config.height / 15,
-            this.game.config.height / 4,
+            this.game.config.height / 4
           ),
-          'airplane',
+          "airplane",
           400,
-          700,
+          700
         );
         strayPlane.setScale(Phaser.Math.Between(5, 10) * 0.05);
         this.strayPlanes.add(strayPlane);
@@ -90,9 +92,9 @@ class GamePlay extends Phaser.Scene {
           this,
           bombingPlane.x,
           bombingPlane.y,
-          'enemyBomb',
+          "enemyBomb",
           Math.cos(angle) * speed,
-          Math.sin(angle) * speed,
+          Math.sin(angle) * speed
         );
         this.enemyBombs.add(bomb);
       },
@@ -104,13 +106,13 @@ class GamePlay extends Phaser.Scene {
       this,
       this.game.config.width / 2.5,
       this.game.config.height * 0.9,
-      'rocketLauncher',
-      20,
+      "rocketLauncher",
+      20
     );
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.keySpace = this.input.keyboard.addKey(
-      Phaser.Input.Keyboard.KeyCodes.SPACE,
+      Phaser.Input.Keyboard.KeyCodes.SPACE
     );
 
     this.physics.add.collider(
@@ -121,7 +123,7 @@ class GamePlay extends Phaser.Scene {
           plane.explode();
           missile.destroy();
         }
-      },
+      }
     );
 
     this.physics.add.collider(this.enemyBombs, this.player, (bomb, player) => {
@@ -134,14 +136,14 @@ class GamePlay extends Phaser.Scene {
     this.reloadMsg = this.add.text(
       this.game.config.width * 0.5,
       this.game.config.height * 0.6,
-      '',
+      "",
       {
-        fontFamily: 'monospace',
+        fontFamily: "monospace",
         fontSize: 30,
-        fontStyle: 'bold',
-        color: 'red',
-        align: 'center',
-      },
+        fontStyle: "bold",
+        color: "red",
+        align: "center",
+      }
     );
 
     this.reloadMsg.setOrigin(0.5);
@@ -155,13 +157,13 @@ class GamePlay extends Phaser.Scene {
       this.player.moveRight();
     }
     if (this.keySpace.isDown) {
-      this.player.setData('isShooting', true);
+      this.player.setData("isShooting", true);
     } else {
       this.player.setData(
-        'timerShootTick',
-        this.player.getData('timerShootDelay') - 1,
+        "timerShootTick",
+        this.player.getData("timerShootDelay") - 1
       );
-      this.player.setData('isShooting', false);
+      this.player.setData("isShooting", false);
     }
     applyFrustumCulling(this, this.playerMissiles.getChildren());
     applyFrustumCulling(this, this.strayPlanes.getChildren());
