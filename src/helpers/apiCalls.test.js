8c2b5dff19f77ef4428970af920f3f9ca02cea6a
Paul Balitema Kabali <engineerbpk@gmail.com>
2021-01-06 01:13:47 +0300

    Add tests for API call functions

---
+++ src/helpers/apiCalls.test.js
@@ -0,0 +1,25 @@
+import getScores, { postScore } from "./apiCalls";
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({ result: { user: "Jane", score: 12 } }),
+  })
+);
+const URL = "www.xxx.nm";
+const ID = "1234";
+const score = {
+  user: "Doe",
+  score: 25,
+};
+
+describe("getScores", () => {
+  it("makes a fetch API call", () => {
+    expect(getScores(URL, ID) instanceof Promise).toBeTruthy();
+  });
+});
+
+describe("postScore", () => {
+  it("makes a fetch API call", () => {
+    expect(fetch).toHaveBeenCalled();
+  });
+});
